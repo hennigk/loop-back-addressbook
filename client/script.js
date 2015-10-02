31884457547b197d6fabf3e178e5351400ca4fd9
hennigk <kayla.hennig@gmail.com>
2015-10-02 02:32:23 +0000

    added form

--- client/script.js
+++ client/script.js
@@ -6,3 +6,29 @@ $.ajax({
         $("#api").append(JSON.stringify(data[i], null, "<br>") + "<br><br>");
     }
 })
+
+
+$('#button').click(function() {
+    var $inputs = $('#form :input');
+    var values = {};
+    $('#form').val("");
+    $inputs.each(function() {
+        if ($(this).val()) {
+        values[this.name] = $(this).val();
+        }
+    });
+    values.addressBookId = "1";
+    console.log(values);
+    $.ajax({
+        url: "https://loop-back-demo-hennigk.c9.io/api/Entries",
+        type: "post",
+        data: values,
+        dataType: 'html',
+    }).done(function(response){
+        $("#addedEntry").append(response + "<br>");
+        $("#inputs :input").each(function(){
+            $(this).val('');
+        });
+        
+    })
+});
